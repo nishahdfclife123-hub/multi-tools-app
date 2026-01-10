e959d7afa24c2fef1fb2bd35e589a9f453154227
nishahdfclife123-hub <nishahdfclife123@gmail.com>
2026-01-10 13:44:15 +0530

    Create home_screen.dart

---
+++ lib/lib/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Multi Tools App'),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(12),
+        children: [
+          toolCard(context, 'Calculator'),
+          toolCard(context, 'Age Calculator'),
+          toolCard(context, 'EMI Calculator'),
+          toolCard(context, 'BMI Calculator'),
+        ],
+      ),
+    );
+  }
+
+  Widget toolCard(BuildContext context, String title) {
+    return Card(
+      elevation: 4,
+      child: Center(
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
